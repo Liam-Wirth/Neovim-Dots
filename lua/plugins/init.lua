34e77dde6060ebc83f0848c6237dabaca24158a9
liam <liam.wirth1@outlook.com>
2022-10-24 01:28:43 -0700

    setup mason

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,7 @@
+--warn
 return require'packer'.startup(function()
     --Packer
     use 'wbthomason/packer.nvim'
-
-
     --Nvim-Tree
     use 'nvim-tree/nvim-web-devicons' -- optional, for file icons
     use 'nvim-tree/nvim-tree.lua'
@@ -10,27 +9,22 @@ return require'packer'.startup(function()
     --telescope
     use 'nvim-telescope/telescope.nvim'
     use 'nvim-lua/plenary.nvim'
-    
     --themes
     use 'EdenEast/nightfox.nvim'
     use 'folke/tokyonight.nvim'
-    
     --bufferline
     use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'kyazdani42/nvim-web-devicons'}
-    
     --autopairs
     use {
 	    "windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
     }
-   
     --lualine
     use 'nvim-lualine/lualine.nvim'
-
     --notify
     use 'rcarriga/nvim-notify'
-    
     --lsp-config & mason
     use 'neovim/nvim-lspconfig'
     use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -84,6 +84,11 @@ _G.packer_plugins = {
     path = "/home/liam/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
+  ["mason-lspconfig.nvim"] = {
+    loaded = true,
+    path = "/home/liam/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    url = "https://github.com/williamboman/mason-lspconfig.nvim"
+  },
   ["mason.nvim"] = {
     loaded = true,
     path = "/home/liam/.local/share/nvim/site/pack/packer/start/mason.nvim",
