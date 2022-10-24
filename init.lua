da981cb29044b8b88181ad986cceef8a53a1643d
liam <liam.wirth1@outlook.com>
2022-10-24 01:07:02 -0700

    setting up mason

--- init.lua
+++ init.lua
@@ -3,7 +3,8 @@ require('plugins')
     require('plugins.nvim-tree-config')
     require('plugins.bufferline')
     require('plugins.lualine')
-    
+    require('lsp-config')
+        require('lsp-config.mason')
 require('colorschemes-config.tokyonight')
 
 
