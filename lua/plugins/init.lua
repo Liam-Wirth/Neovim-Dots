da1ad41f7b9c01450a62d7372cce4cfe81adc4cc
liam <liam.wirth1@outlook.com>
2022-10-25 14:09:14 -0700

    completion?

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,4 @@
---War
+local use = require('packer').use
 
 return require'packer'.startup(function()
     --Packer
@@ -30,12 +30,11 @@ return require'packer'.startup(function()
     use 'williamboman/mason-lspconfig.nvim'
     use 'mfussenegger/nvim-dap'
     use 'jose-elias-alvarez/null-ls.nvim'
-    use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
-    use 'L3MON4D3/LuaSnip'
-    use 'saadparwaiz1/cmp_luasnip'
-
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
 end)
