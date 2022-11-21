1f8b57ea02f44037af419be62f7adc6b54c29cac
liam <liam.wirth1@outlook.com>
2022-11-20 21:53:04 -0700

    :)

--- init.lua
+++ init.lua
@@ -4,14 +4,10 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 require('plugins')
-    require('plugins.lookandfeel.bufferline')
-    require('plugins.lookandfeel.lualine')
-    require('plugins.lookandfeel.notify')
     require('plugins.lookandfeel.nvimtree')
-    require('plugins.lookandfeel.alpha')
     require('plugins.discordrpc')
 
-
+require('settings.init')
 
 
 require('plugins.treesitter')
@@ -19,17 +15,8 @@ require('plugins.treesitter')
         require('lsp-config.mason')
         require('lsp-config.lsp-config')
         require('lsp-config.null-ls')
-        require('lsp-config.nvim-cmp')
-        require('lsp-config.luasnip')
-
 local vim = vim;
 
 vim.cmd("colorscheme doom-one")
 --HACK way of fucking loading configuration files. Need to fix
-vim.cmd([[
-    luafile ~/.config/nvim/lua/settings/lookandfeel.lua
-    luafile ~/.config/nvim/lua/settings/init.lua
-]])
---TODO
---HACKS
 --
