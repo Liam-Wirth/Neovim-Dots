4e46972eab746b455f7a75f20638eaf19877dd80
liam <liam.wirth1@outlook.com>
2022-11-21 15:42:27 -0700

    fixed some minor bugs in my cmp configuration

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,4 +1,32 @@
+
 -- Add additional capabilities supported by nvim-cmp
+local kind_icons = {
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local luasnip = require'luasnip'
 local has_words_before = function()
@@ -23,59 +51,75 @@ cmp.setup {
 completion = {
     completeopt = "menu,menuone,noinsert",
   },
- 
+
   snippet = {
     expand = function(args)
       require("luasnip").lsp_expand(args.body)
     end,
   },
-
 experimental = {
     ghost_text = true,
+    native_menu = false,
   },
 window = {
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
   },
-  view = {
-    native_menu = true,
-  },
-
  -----------------------------------------------------------------------------------------------------------------------------------------
-  --                                                               Mappings                                                              --  
+  --                                                               Mappings                                                              --
   -----------------------------------------------------------------------------------------------------------------------------------------
 
-   mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = false}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+ --  mapping = cmp.mapping.preset.insert({
+ --     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+ --     ['<C-f>'] = cmp.mapping.scroll_docs(4),
+ --     ['<C-Space>'] = cmp.mapping.complete(),
+ --     ['<C-e>'] = cmp.mapping.abort(),
+ --     ['<CR>'] = cmp.mapping.confirm({ select = false}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+ --   ["<Tab>"] = cmp.mapping(function(fallback)
+ --     if cmp.visible() then
+ --       cmp.select_next_item()
+ --     elseif luasnip.expand_or_jumpable() then
+ --       luasnip.expand_or_jump()
+ --     elseif has_words_before() then
+ --       cmp.complete()
+ --     else
+ --       fallback()
+ --     end
+ --   end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
-
+ --   ["<S-Tab>"] = cmp.mapping(function(fallback)
+ --     if cmp.visible() then
+ --       cmp.select_prev_item()
+ --     elseif luasnip.jumpable(-1) then
+ --       luasnip.jump(-1)
+ --     else
+ --       fallback()
+ --     end
+ --   end, { "i", "s" }),
+ -- }),
+ mapping = {
+    ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { "i", "c" }),
+    ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { "i", "c" }),
+    ["<C-j>"] =  cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { "i", "c" }),
+    ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ["<C-e>"] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ["<CR>"] = cmp.mapping(
+      cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      }),
+      { "i", "c" }
+    ),
+  },
   -----------------------------------------------------------------------------------------------------------------------------------------
-  --                                                               Sources                                                               --  
+  --                                                               Sources                                                               --
   -----------------------------------------------------------------------------------------------------------------------------------------
   sources = {
     { name = 'nvim_lsp' },
@@ -83,11 +127,15 @@ window = {
     { name = 'java'},
     { name = 'orgmode'},
     { name = "omni" },
-    {
+      {
       name = "buffer",
       option = {
         get_bufnrs = function()
-          return vim.api.nvim_list_bufs()
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            bufs[vim.api.nvim_win_get_buf(win)] = true
+          end
+          return vim.tbl_keys(bufs)
         end,
       },
     },
@@ -98,7 +146,23 @@ window = {
     { name = "calc" },
     { name = "vim-dadbod-completion" },
   },
-
+formatting = {
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      -- Source
+      vim_item.menu = ({
+        buffer = "",
+        nvim_lsp = "",
+        spell = "",
+        look = "",
+        unit = "",
+        luasnip = "[LuaSnip]",
+        nvim_lua = "[Lua]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
 }
 cmp.setup.cmdline(":", {
   mapping = {
@@ -109,7 +173,7 @@ cmp.setup.cmdline(":", {
     ["<TAB>"] = cmp.mapping({
       c = cmp.mapping.confirm({ select = true }),
     }, { "i", "c" }),
-    ["<CR>"] = cmp.mapping({
+    ["<S-CR>"] = cmp.mapping({
       i = cmp.mapping.confirm({ select = true }),
     }, { "i", "c" }),
   },
