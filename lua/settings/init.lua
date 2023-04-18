91abda60a438441d629a4d666225c72cf7c27506
liam <liam.wirth1@outlook.com>
2023-04-18 15:58:53 -0700

    i swear, the problems I'm having with doom-one are gonna be the end of me

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -85,3 +85,26 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
+-------------------------------------------------------------------------------------------------------------
+--                                                    doom-one                                              -- --------------------------------------------------------------------------------------------------------------
+--
+vim.g.doom_one_cursor_coloring = false
+vim.g.doom_one_terminal_colors = true
+vim.g.doom_one_italic_comments = false
+vim.g.doom_one_enable_treesitter = true
+vim.g.doom_one_diagnostics_text_color = false
+vim.g.doom_one_transparent_background = false
+vim.g.doom_one_pumblend_enable = false
+vim.g.doom_one_pumblend_transparency = 20
+vim.g.doom_one_plugin_neorg = true
+vim.g.doom_one_plugin_barbar = false
+vim.g.doom_one_plugin_telescope = false
+vim.g.doom_one_plugin_neogit = true
+vim.g.doom_one_plugin_nvim_tree = true
+vim.g.doom_one_plugin_dashboard = true
+vim.g.doom_one_plugin_startify = true
+vim.g.doom_one_plugin_whichkey = true
+vim.g.doom_one_plugin_indent_blankline = true
+vim.g.doom_one_plugin_vim_illuminate = true
+vim.g.doom_one_plugin_lspsaga = false
+
